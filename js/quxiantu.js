dd939b7ffc03a3ad60c0b0a23e3343392856c59f
caiwei <1392960408@qq.com>
2019-02-04 14:03:20 +0800

    first commit

---
+++ js/quxiantu.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Administrator on 2017\11\27 0027.
+ */
+var data = {
+    labels : ["周日","周一","周二","周三","周四","周五","周六"],
+    datasets : [
+        {
+            fillColor : "rgba(220,220,220,0.5)",  /*统计表的背景色*/
+            strokeColor : "rgba(220,220,220,1)",   /*画笔的颜色*/
+            pointColor : "rgba(220,220,220,1)",   /*点的颜色*/
+            pointStrokeColor : "#fff",      /*鼠标触发的颜色*/
+            data : [65,59,90,81,56,55,40]     /*坐标数据*/
+        },
+        {
+            fillColor : "rgba(151,187,205,0.5)",
+            strokeColor : "rgba(151,187,205,1)",
+            pointColor : "rgba(151,187,205,1)",
+            pointStrokeColor : "#fff",
+            data : [28,48,40,19,96,27,100]
+        }
+    ]
+}
